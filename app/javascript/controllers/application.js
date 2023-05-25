ad5bf3a30ebcbba24fc0653a61c2026abac3e137
Rory Boyle <rory.boyle13@yahoo.co.uk>
2023-05-25 11:55:19 +0100

    movie make gem install etc

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-
+import "bootstrap"
+import "@popperjs/core"
 const application = Application.start()
 
 // Configure Stimulus development experience
